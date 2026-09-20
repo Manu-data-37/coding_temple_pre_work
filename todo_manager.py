037421b85d633fb4f50d440e9f1f9af8cf98ee26
Manuel Marquez <mmarquezlop.99@gmail.com>
2026-09-20 13:37:46 -0600

    This is the first version of a taks manager i am working on. My next step is to add error handling

---
+++ todo_manager.py
@@ -0,0 +1,27 @@
+# 1. Start with a pre-populated list of 3 tasks
+list_of_tasks = ["Walk the dog", "Clean room", "wash dishes"]
+
+# 2. Display the current to-do list (numbered, starting from 1)s
+print('Current List')
+for number,task in enumerate(list_of_tasks, start=1):
+ print(f'{number} {list_of_tasks}')
+
+print("Number of current tasks")
+print(len(list_of_tasks))
+
+# 3. Let the user add a new task (append)
+user_input = input("Enter a new task: ")
+list_of_tasks.append(user_input)
+
+# 4. Let the user remove a task by number (pop)
+remove_number = int(input("Enter the number of the task you want to remove"))
+remove_task = list_of_tasks.pop(remove_number-1)
+print(f'You removed: ', {remove_task})
+
+# 5. Display the updated list after each operation
+print('Updated List')
+for number,task in enumerate(list_of_tasks, start=1):
+  print(f'{number} {list_of_tasks}')
+
+# 6. Show the total number of tasks remaining
+print(f'Number of current tasks {len(list_of_tasks)}')
