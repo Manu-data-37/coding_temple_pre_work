35957a44a6cd390ffc609a3f2100fe0ca90a53b3
Manuel Marquez <mmarquezlop.99@gmail.com>
2026-09-20 20:40:27 -0600

    A cleaner version of the code has been written. I still need to add better error handling code

--- todo_manager.py
+++ todo_manager.py
@@ -1,39 +1,47 @@
-# 1. Start with a pre-populated list of 3 tasks
-list_of_tasks = ["Walk the dog", "Clean room", "wash dishes"]
+# TODO: Start with a pre-populated list of 3 tasks
+# TODO: Display the current to-do list (numbered, starting from 1)
+# TODO: Let the user add a new task (append)
+# TODO: Let the user remove a task by number (pop)
+# TODO: Display the updated list after each operation
+# TODO:Show the total number of tasks remaining
 
-# 2. Display the current to-do list (numbered, starting from 1)s
-print('Current List')
-for number,task in enumerate(list_of_tasks, start=1):
- print(f'{number} {list_of_tasks}')
 
-print("Number of current tasks")
-print(len(list_of_tasks))
+# DONE: Start with a pre-populated list of 3 tasks
+list_of_tasks = ["Clean room", "Go to the doctor", "Washo dishes"]
 
-# 3. Let the user add a new task (append)
-user_input = input("Enter a new task: ")
-list_of_tasks.append(user_input)
+# DONE: Display the current to-do list (numbered, starting from 1)
+for number, task in enumerate(list_of_tasks, start=1):
+  print(f'{number} {task}')
 
-# 4. Let the user remove a task by number (pop)
-# Add error handling: We are given ValueError
-# The plan of attack is...
-'''
-try:
-  CODE. 
-except ErrorMessage:
-  print('This message is triggred if the error occurs')
 
-'''
-try:
-    remove_number = int(input("Enter the number of the task you want to remove"))
-    remove_task = list_of_tasks.pop(remove_number-1)
-    print(f'You removed: ', {remove_task})
-except ValueError:
-  print('You need to type the number of the task you want to remove. Do not type text.')
+# TODO: Let the user add a new task (append)
+print('Do you want to ADD or REMOVE a task?')
+user_input = input('Type what you want to do')
 
-# 5. Display the updated list after each operation
-print('Updated List')
-for number,task in enumerate(list_of_tasks, start=1):
-  print(f'{number} {list_of_tasks}')
+if user_input.upper() == 'ADD':
+  new_task = input('What do you want to add?')
+  list_of_tasks.append(new_task)
+  print(f'Added: {new_task}')
 
-# 6. Show the total number of tasks remaining.
-print(f'Number of current tasks {len(list_of_tasks)}')
+# TODO: Let the user remove a task by number (pop)
+
+elif user_input.upper() == 'REMOVE':
+  try:
+    remove_task = int(input('Enter the number of the task you want to remove'))
+    removed_task = list_of_tasks.pop(remove_task - 1)
+    print(f'Removed: {remove_task}')
+  except ValueError:
+    print('Type a number, not  text')
+  except IndexError:
+    print('That number is not on the list')
+else:
+  print('An error has occured. Restart the program!')
+
+# TODO: Display the updated list after each operation
+# TODO:Show the total number of tasks remaining
+
+# Updated list of tasks
+print(f'Your tasks are: {len(list_of_tasks)}')
+print(f'Updated list of tasks {list_of_tasks}')
+
+
